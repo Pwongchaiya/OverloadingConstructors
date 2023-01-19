68f9741884338ae3e282bba0418d81bf24b3ce98
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-01-18 20:29:52 -0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PotentialEmployee john = new PotentialEmployee("John","Doe","johndoe@doe.com", "778989865");
+        PotentialEmployee employee = new PotentialEmployee("johndoe@doe.com", "778989865");
+        PotentialEmployee email = new PotentialEmployee("johndoe@doe.com");
+    }
+}
